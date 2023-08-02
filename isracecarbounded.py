0f745a61b2d2b6449225c9d4606169090e570e00
AaronKhazan <138054478+AaronKhazan@users.noreply.github.com>
2023-08-02 09:14:35 -0400

    Update isracecarbounded.py

--- isracecarbounded.py
+++ isracecarbounded.py
@@ -72,7 +72,7 @@ class Solution:
                       L+=1
                 if i == "R":
                       R+=1
-            if R == L:
+            if R == L == G:
                 return True
             else:
                 return False
